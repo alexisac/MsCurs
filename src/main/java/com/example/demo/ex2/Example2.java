770b0c383d56a1df9d5cc4e01d000638485de134
alex <alex_isac72@yahoo.com>
2024-10-19 13:36:39 +0300

    Example 2 2

---
+++ src/main/java/com/example/demo/ex2/Example2.java
@@ -0,0 +1,13 @@
+package com.example.demo.ex2;
+
+public class Example2 {
+    public int b = 20;
+
+    public int a = 10;
+
+    public int c = 100;
+
+    public String e = "something";
+
+    public int d = 30;
+}
