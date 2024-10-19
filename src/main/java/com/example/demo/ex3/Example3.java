a0429f67b24310e4f17c97dfe1d98a868bd96415
alex <alex_isac72@yahoo.com>
2024-10-19 13:40:39 +0300

    Example 3 1

--- src/main/java/com/example/demo/ex3/Example3.java
+++ src/main/java/com/example/demo/ex3/Example3.java
@@ -1,13 +1,13 @@
 package com.example.demo.ex3;
 
 public class Example3 {
-    int a = 10;
-
     int b = 20;
+
+    int a = 10;
 
     int c = 30;
 
-    int d = 40;
-
     int e = 50;
+
+    int d = 40;
 }
